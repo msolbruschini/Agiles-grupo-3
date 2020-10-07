9f8538f85a26d055b76be96ecb953495ffa61ec5
NahuelCad <nahuel.cad@gmail.com>
2020-10-07 19:36:12 -0300

    Add gridView for stock

--- Interfez/Stock.Designer.cs
+++ Interfez/Stock.Designer.cs
@@ -28,51 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // textBox1
             // 
-            this.button1.BackColor = System.Drawing.Color.OrangeRed;
-            this.button1.Location = new System.Drawing.Point(341, 70);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 44);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Ver Stock";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox1.Location = new System.Drawing.Point(201, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
             // 
-            // button2
+            // contextMenuStrip1
             // 
-            this.button2.BackColor = System.Drawing.Color.OrangeRed;
-            this.button2.Location = new System.Drawing.Point(341, 143);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 41);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Modificar Stock";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(201, 215);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(426, 266);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Stock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.BackgroundImage = global::Interfez.Properties.Resources.cerveceria;
-            this.ClientSize = new System.Drawing.Size(805, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(1216, 634);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Stock";
             this.Text = "Stock";
             this.Load += new System.EventHandler(this.Stock_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Interfez/Stock.cs
+++ Interfez/Stock.cs
@@ -6,12 +6,69 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Interfez
 {
+
+
+
     public partial class Stock : Form
     {
+
+
+        public class Helper
+        {
+            public static DataTable DataTableFromTextFile(string location, char delimiter = ',')
+            {
+                DataTable result;
+
+                string[] LineArray = File.ReadAllLines(location);
+
+                result = FormDataTable(LineArray, delimiter);
+
+                return result;
+            }
+
+            private static DataTable FormDataTable(string[] LineArray, char delimiter)
+            {
+                DataTable dt = new DataTable();
+
+                AddColumnToTable(LineArray, delimiter, ref dt);
+
+                AddRowToTable(LineArray, delimiter, ref dt);
+
+                return dt;
+            }
+
+            private static void AddRowToTable(string[] valueCollection, char delimiter, ref DataTable dt)
+            {
+
+                for (int i = 1; i < valueCollection.Length; i++)
+                {
+                    string[] values = valueCollection[i].Split(delimiter);
+                    DataRow dr = dt.NewRow();
+                    for (int j = 0; j < values.Length; j++)
+                    {
+                        dr[j] = values[j];
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+
+            private static void AddColumnToTable(string[] columnCollection, char delimiter, ref DataTable dt)
+            {
+                string[] columns = columnCollection[0].Split(delimiter);
+                foreach (string columnName in columns)
+                {
+                    DataColumn dc = new DataColumn(columnName, typeof(string));
+                    dt.Columns.Add(dc);
+                }
+            }
+
+        }
+
         public Stock()
         {
             InitializeComponent();
@@ -29,7 +86,12 @@ namespace Interfez
 
         private void Stock_Load(object sender, EventArgs e)
         {
+            dataGridView1.DataSource = Helper.DataTableFromTextFile(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "Testing.txt"), '|');
+        }
 
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
         }
     }
 }
